c3005d3d49c2b9ed038afefdff51b87544886004
VasiliiChibisov <chibisowasya@gmail.com>
2022-03-06 12:53:34 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace FirstApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string myName;
+            myName = "Vasilii";
+
+            Console.WriteLine(myName);
+
+            Console.ReadKey();
+        }
+    }
+}
